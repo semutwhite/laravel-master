c59cb3877de4f2f8f612578278648249f18b420b
The White Ants <thewhiteants@gmail.com>
2024-06-03 04:17:32 +0700

    coding layout arrangement

--- vite.config.ts
+++ vite.config.ts
@@ -3,41 +3,41 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.tsx',
-            refresh: true,
-        }),
-        react(),
-        splitVendorChunkPlugin(),
-    ],
-    resolve: {
-        alias: {
-            '@': '/resources/js',
-            '@css': '/resources/css',
-        },
+  plugins: [
+    laravel({
+      input: 'resources/js/app.tsx',
+      refresh: true,
+    }),
+    react(),
+    splitVendorChunkPlugin(),
+  ],
+  resolve: {
+    alias: {
+      '@': '/resources/js',
+      '@css': '/resources/css',
     },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(_id) {
-                    if (_id.includes('lodash')) {
-                        return 'lodash';
-                    }
-                    if (_id.includes('ably')) {
-                        return 'ably';
-                    }
-                    if (_id.includes('cypress')) {
-                        return 'cypress';
-                    }
-                    if (_id.includes('react') || _id.includes('react-dom') || _id.includes('react-type-animation')) {
-                        return 'react';
-                    }
-                    if (_id.includes('leaflet') || _id.includes('react-leaflet')) {
-                        return 'mapjs';
-                    }
-                },
-            },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(_id) {
+          if (_id.includes('lodash')) {
+            return 'lodash';
+          }
+          if (_id.includes('ably')) {
+            return 'ably';
+          }
+          if (_id.includes('cypress')) {
+            return 'cypress';
+          }
+          if (_id.includes('react') || _id.includes('react-dom') || _id.includes('react-type-animation')) {
+            return 'react';
+          }
+          if (_id.includes('leaflet') || _id.includes('react-leaflet')) {
+            return 'mapjs';
+          }
         },
+      },
     },
+  },
 });
