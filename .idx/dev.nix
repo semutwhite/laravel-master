2cd5c3450161edb51d39bacd074f583216da2c75
semutputih.2024 <semutputih.2024@gmail.com>
2024-06-03 02:14:44 +0000

    create idx google

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.php82
+    pkgs.php82Packages.composer
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "php"
+          "artisan"
+          "serve"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
